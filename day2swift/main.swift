5fbcb56e23b2e806b07ee09ac9970f4bcb07ca9e
varinderdhillon123 <varinder701dhillon@gmail.com>
2019-10-09 18:02:29 -0400

    day2

---
+++ day2swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  day2swift
+//
+//  Created by MacStudent on 2019-10-09.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
