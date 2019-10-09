508538423e3d543a7d62ef4ed3d7392136622bf7
varinderdhillon123 <varinder701dhillon@gmail.com>
2019-10-09 18:17:30 -0400

    day2swift

--- day2swift/main.swift
+++ day2swift/main.swift
@@ -10,4 +10,36 @@ import Foundation
 
 print("Hello, World!")
 
-var a = 10
+
+let str = "aaabbccccdeeeab"
+let array = Array(str)
+var finalStr = String()
+finalStr = String(array[0])
+var counter = 1;
+for i in 0..<array.count
+{
+    if(i+1) < array.count
+    {
+        if array[i] == array[i+1]
+        {
+            counter = counter + 1;
+        }
+        else
+        {
+            if(counter != 1)
+            {
+                finalStr = "\(finalStr)\(counter)\(array[i+1])"
+                counter = 1;
+            }
+            else
+            {
+                finalStr = "\(finalStr)\(array[i+1])"
+                counter = 1;
+            }
+        }
+    }
+}
+print(finalStr)
+
+
+
